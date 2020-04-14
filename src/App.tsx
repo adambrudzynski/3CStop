3ec5bd23ce52e59094132e636a9a4aa353cd874b
Adam Brudzynski <adam.brudzynski@gmail.com>
2020-04-14 14:10:20 +0200

    Error Boiundary implementtion

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './app.css'
 import Content from './Components/Content';
+import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
 
 
-const App =() =>{
+const App = () => {
   return (
+    <ErrorBoundary >
       <Content key='list' />
+    </ErrorBoundary>
   );
 }
 
